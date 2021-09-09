ffe8629f89fae5fdafdf32865669c69ed6991168
JhonMos <jhonmos14@hotmail.com>
2021-09-09 01:49:03 -0500

    responsive details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,37 +1,42 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router";
 import { DataContext } from "./DataProvider.js";
 import styles from "./Details.module.css";
 
 export const Details = () => {
   const { id } = useParams();
-  const [products, setProducts] = useContext(DataContext);
+  const [products] = useContext(DataContext);
 
-  const details = products.filter(
-    (product, index) => product.id === parseInt(id)
-  );
+  const details = products.filter((product) => product.id === parseInt(id));
 
   return (
     <>
       {details.map((product) => (
-        <div className="details" key={product.id}>
+        <div className={styles.details} key={product.id}>
           <div
             className={styles.imgContainer}
             style={{ backgroundImage: `url(${product.image})` }}
           />
-          <div className="boxDetails">
+          <div className={styles.boxDetails}>
             <p>{product.brand}</p>
             <h2>{product.name}</h2>
-            <h3>COP {product.price}</h3>
-            <p>Información del producto</p>
+            <div className={styles.prueba}>
+              <h3>COP {product.price}</h3>
+              <div className={styles.counterContainer}>
+                <button className={styles.btnPlus}>-</button>
+                <span className={styles.quantity}>1</span>
+                <button className={styles.btnLess}>+</button>
+              </div>
+            </div>
+            <p className={styles.infoProduct}>Información del producto</p>
             <ul>
-              {
-                  product.description.map((description, index) =>(
-                      <li key={index}>- {description}</li>
-                  ))
-              }
+              {product.description.map((description, index) => (
+                <li key={index}>- {description}</li>
+              ))}
             </ul>
-            <button>Agregar al carrito</button>
+            <div className={styles.containerAddCart}>
+              <button className={styles.addCart}>Agregar al carrito</button>
+            </div>
           </div>
         </div>
       ))}
